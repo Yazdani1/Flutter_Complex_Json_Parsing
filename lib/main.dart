77e9f081c5fcf4e27ee4965c41c60b6c39d99e2b
Yazdani <yaz4dani@gmail.com>
2019-05-18 22:13:17 +0600

    Second

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,13 @@ import 'package:http/http.dart' as http;
 
 void main() async{
 
-  List m=await getData();
+  List data=await getComplex();
+
+  //print(data[0]["address"]["geo"]["lat"]);
+  print(data[0]["address"]["city"]);
+  
+
+//  List m=await getData();
 
 //  List<String>listOf=[];
 //  int i;
@@ -22,60 +28,75 @@ void main() async{
         backgroundColor: Colors.orange,
       ),
 
-    body: new ListView.builder(
-        itemCount: m.length,
-      itemBuilder: (BuildContext c,int index){
-          return Card(
-            elevation: 10.0,
-            child: new Row(
-              children: <Widget>[
-
-                new Expanded(
-                    flex: 1,
-                  child: new CircleAvatar(
-                    child: new Text(m[index]["id"].toString()[0]),
-                    backgroundColor: Colors.red,
-                    foregroundColor: Colors.white,
-                  ),
-                ),
-                new SizedBox(width: 5.0,),
-                new Expanded(
-                  flex: 3,
-                  child: InkWell(
-                    child: new Text(m[index]["title"],
-                    maxLines: 2,
-                    style: TextStyle(
-                      fontSize: 22.0
-                    ),
-                    ),
-                    onTap: (){
-                      print("Data is ${m[index]["userId"]}");
-                    },
-                  ),
-                ),
-                
 
 
-              ],
-            ),
-          );
-      }
-    ),
+//    body: new ListView.builder(
+//        itemCount: m.length,
+//      itemBuilder: (BuildContext c,int index){
+//          return Card(
+//            elevation: 10.0,
+//            child: new Row(
+//              children: <Widget>[
+//
+//                new Expanded(
+//                    flex: 1,
+//                  child: new CircleAvatar(
+//                    child: new Text(m[index]["id"].toString()[0]),
+//                    backgroundColor: Colors.red,
+//                    foregroundColor: Colors.white,
+//                  ),
+//                ),
+//                new SizedBox(width: 5.0,),
+//                new Expanded(
+//                  flex: 3,
+//                  child: InkWell(
+//                    child: new Text(m[index]["title"],
+//                    maxLines: 2,
+//                    style: TextStyle(
+//                      fontSize: 22.0
+//                    ),
+//                    ),
+//                    onTap: (){
+//                      print("Data is ${m[index]["userId"]}");
+//                    },
+//                  ),
+//                ),
+//
+//
+//
+//              ],
+//            ),
+//          );
+//      }
+//    ),
 
     ),
   ));
 }
-Future<List> getData() async{
 
-  String url = "https://jsonplaceholder.typicode.com/posts";
+Future<List>getComplex() async{
+  String api="https://jsonplaceholder.typicode.com/users";
 
-  var data=await http.get(url);
+  var data=await http.get(api);
   var jsonData=json.decode(data.body);
 
   return jsonData;
 
-//  http.Response response=await http.get(url);
-//  return response.body;
-
-
 }
+
+
+
+//Future<List> getData() async{
+//
+//  String url = "https://jsonplaceholder.typicode.com/posts";
+//
+//  var data=await http.get(url);
+//  var jsonData=json.decode(data.body);
+//
+//  return jsonData;
+//
+////  http.Response response=await http.get(url);
+////  return response.body;
+//
+//
+//}
